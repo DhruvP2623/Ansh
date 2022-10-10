728187667d7f6f7fd9b6d9e71e3c71623daaa219
Dhruv Patel <dhruvp6400@gmail.com>
2022-10-10 21:53:52 +0100

    ADDDD commit

---
+++ src/ADDD.java
@@ -0,0 +1,14 @@
+public class ADDD
+{
+    static int number = 10;
+
+
+    public static void main(String[] args) {
+
+        String output = number % 2 ==0 ? "Even" : "Odd";
+
+        System.out.println(number +" is " + output );
+
+
+    }
+}
