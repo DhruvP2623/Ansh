1809867695886efe301aef40498838fed45ff57d
Dhruv Patel <dhruvp6400@gmail.com>
2022-10-10 21:55:49 +0100

    dddd commit

---
+++ src/DDDD.java
@@ -0,0 +1,17 @@
+public class DDDD {
+
+    static int year = 2011;
+
+    public static void main(String[] args){
+
+
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year % 400 == 0))
+        {
+            System.out.println(year + " is leap year.");
+        }
+        else
+        {
+            System.out.println(year + " is not leap year.");
+        }
+    }
+}
